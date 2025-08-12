ed51a5547640cc81bffbd87d7654ac1b3ee8b1c1
Rishank Pandey <rishankpandeysss01@gmail.com>
2025-08-12 22:47:22 +0530

    add BlurCircle component

--- client/src/component/FeaturedSection.jsx
+++ client/src/component/FeaturedSection.jsx
@@ -1,25 +1,44 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { ArrowRight } from "lucide-react";
+import BlurCircle from "./BlurCircle";
+import { useNavigate } from "react-router-dom";
 
 const FeaturedSection = () => {
-  return (
-    <div className="flex justify-between items-center px-6 md:px-16 lg:px-36 py-6 ">
-      
-      {/* Title */}
-      <h2 className="text-white text-xl font-semibold">
-        Now Showing
-      </h2>
 
-      {/* View All Button */}
-      <Link
-        to="/movies"
-        className="flex items-center gap-2 text-white hover:text-red-500 transition duration-200"
-      >
-        View All <ArrowRight size={16} />
-      </Link>
+  const navigate = useNavigate()
+
+  return (
+    <div className='px-6 md:px-16 lg:px-24 xl:px-44 overflow-hidden'>
+
+      <div className='relative flex items-center justify-between pt-20 pb-10'>
+        <BlurCircle top='0' right='-80px' />
+        
+        <p className='text-gray-300 font-medium text-lg'>Now Showing</p>
+        
+        <button 
+          onClick={() => navigate('/movies')}
+          className='group flex items-center gap-2 text-sm text-gray-300 cursor-pointer'
+        >
+          View All
+          <ArrowRight className='group-hover:translate-x-0.5 transition w-4.5 h-4.5' />
+        </button>
+      </div>
+      <div className='flex justify-center mt-20'>
+  <button 
+    onClick={() => {
+      navigate('/movies');
+      scrollTo(0, 0);
+    }} 
+    className='px-10 py-3 text-sm bg-primary hover:bg-primary-dull transition rounded-md font-medium cursor-pointer'
+  >
+    Show more
+  </button>
+</div>
+
     </div>
-  );
-};
+  )
+}
+
 
 export default FeaturedSection;
